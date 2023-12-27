143ade09b16b5e84f246e93545520afc90b2a6d0
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-27 19:23:34 +0530

    useCase1 done

---
+++ src/main/java/Queue.java
@@ -0,0 +1,54 @@
+public class Queue <T>{
+    Node<T>head;
+    Node<T>tail;
+
+    Queue(){
+        head=null;
+        tail=null;
+    }
+
+    boolean isEmpty(){
+        return head==null;
+    }
+
+    public int size(){
+        int cnt=0;
+        Node<T>temp=head;
+        while(temp!=null){
+            cnt++;
+            temp=temp.next;
+        }
+
+        return cnt;
+    }
+
+    public void enqueue(T data){
+        Node<T>newNode=new Node(data);
+        if(head==null){
+            head=tail=newNode;
+        }else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+
+    public void dequeue(){
+        if(head==null){
+            tail=null;
+            System.out.println("Queue is empty");
+        }else{
+            head=head.next;
+            if(head==null) tail=null;
+        }
+    }
+
+    public void display(){
+        Node<T>temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+
+
+}
---
+++ src/main/java/QueueTest.java
@@ -0,0 +1,11 @@
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<Integer>queue=new Queue<>();
+
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+
+        queue.display();
+    }
+}
